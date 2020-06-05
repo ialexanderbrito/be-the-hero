911570a43780ee86de20e3c728729a468277b66a
Alexander <xanderzinho26@gmail.com>
2020-06-04 23:49:02 -0300

    Adicionado icones WhatsApp e Email

--- mobile/src/pages/Detail/styles.js
+++ mobile/src/pages/Detail/styles.js
@@ -1,78 +1,80 @@
 import { StyleSheet } from 'react-native';
-import Constants from 'expo-constants'
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20,
-    },
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
 
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 
-    incident:{
-        padding: 24,
-        borderRadius: 8,
-        backgroundColor: '#FFF',
-        marginBottom: 16,
-        marginTop: 48,
-    },
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 16,
+    marginTop: 48,
+  },
 
-    incidentProperty: {
-        fontSize: 14,
-        color: '#41414d',
-        fontWeight: 'bold',
-        marginTop: 24,
-    },
+  incidentProperty: {
+    fontSize: 14,
+    color: '#41414d',
+    fontWeight: 'bold',
+    marginTop: 24,
+  },
 
-    incidentValue: {
-        marginTop: 8,
-        fontSize: 15,
-        color: '#737380',
-    },
-    
-    contactBox:{
-        padding: 24,
-        borderRadius: 8,
-        backgroundColor: '#FFF',
-        marginBottom: 16,
-    },
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    color: '#737380',
+  },
 
-    heroTitle: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        color: '#13131a',
-        lineHeight: 30,
-    },
+  contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 16,
+  },
 
-    heroDescription: {
-        fontSize: 15,
-        color: '#737380',
-        marginTop: 16,
-    },
+  heroTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#13131a',
+    lineHeight: 30,
+  },
 
-    actions: {
-        marginTop: 16,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
+  heroDescription: {
+    fontSize: 15,
+    color: '#737380',
+    marginTop: 16,
+  },
 
-    action: {
-        backgroundColor: '#e02041',
-        borderRadius: 8,
-        height: 50,
-        width: '48%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  actions: {
+    marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 
-    actionText: {
-        color: '#FFF',
-        fontSize: 15,
-        fontWeight: 'bold',
-    },     
+  action: {
+    width: '48%',
+    backgroundColor: '#e02041',
+    borderRadius: 8,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  actionText: {
+    marginLeft: 8,
+    color: '#FFF',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
 });
