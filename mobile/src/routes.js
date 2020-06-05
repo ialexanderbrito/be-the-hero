204214065725027126136d26353b599f8dc6ea7d
Alexander <xanderzinho26@gmail.com>
2020-06-04 23:48:25 -0300

    Adicionado animação do iOS no Android

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
 
 const AppStack = createStackNavigator();
 
@@ -8,13 +11,19 @@ import Incidents from './pages/Incidents';
 import Detail from './pages/Detail';
 
 export default function Routes() {
-    return(
-        <NavigationContainer>
-            <AppStack.Navigator screenOptions={{ headerShown: false }}>
-                <AppStack.Screen name="Incidents" component={Incidents} />
-                <AppStack.Screen name="Detail" component={Detail} />
-            </AppStack.Navigator>
-        </NavigationContainer>
-
-    );
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator
+        screenOptions={{
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          headerShown: false,
+        }}
+      >
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Detail" component={Detail} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
 }
